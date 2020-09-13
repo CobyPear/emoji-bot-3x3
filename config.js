aa1f375761f7dc9740447b1f8911c9a539d66aab
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-09-12 20:15:18 -0500

    environmental variables added

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    consumer_key:'WhRDFaXesI4Ua98ayWjWq2Sye',
-    consumer_secret:'cUcFQgmUPI0aiVqlxthMGEAr1fOwj4k3O5CZcJU5HX7tLMLHpw',
-    access_token:'1259943730506641410-rB0DWgM7ZmF68tCmpNCNE98ucDgkIn',
-    access_token_secret:'eA9qzwDfSZOOewD5wuytnhUMYxoYsZogtryarv5z351bY'
+    consumer_key: process.env.KEY,
+    consumer_secret:process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret:process.env.ACCESS_TOKEN_SECRET
     };
